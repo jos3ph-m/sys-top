8d817bb683a81dd4fbc783dce44a821ac5937ddc
Joseph M <jmedina1985@hotmail.com>
2020-06-17 23:05:59 -0700

    Added Store class

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, Menu } = require("electron");
 const log = require("electron-log");
+const Store = require("./Store");
 
 // Set env
 process.env.NODE_ENV = "production";
@@ -8,6 +9,17 @@ const isDev = process.env.NODE_ENV !== "production" ? true : false;
 const isMac = process.platform === "darwin" ? true : false;
 
 let mainWindow;
+
+// Init store & defaults
+const store = new Store({
+  configName: "user-settings",
+  defaults: {
+    settings: {
+      cpuOverload: 80,
+      alertFrequency: 5,
+    },
+  },
+});
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
