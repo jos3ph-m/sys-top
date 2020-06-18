d0c589fb64cbc79aa9d4192fb15b85d41fcad920
Joseph M <jmedina1985@hotmail.com>
2020-06-17 23:13:33 -0700

    Set default settings in renderer updated

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,5 +1,6 @@
 const { ipcRenderer } = require("electron");
 
+// Set default settings
 ipcRenderer.on("settings:get", (e, settings) => {
   document.getElementById("cpu-overload").value = settings.cpuOverload;
   document.getElementById("alert-frequency").value = settings.alertFrequency;
